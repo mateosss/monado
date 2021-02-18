3d33c001449ebe2f40f8404709f5661e89e4a3de
Mateo de Mayo <mateodemayo@gmail.com>
2021-02-17 22:03:55 -0300

    Add qwerty driver references

---
+++ src/xrt/drivers/qwerty/qwerty_interface.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define QWERTY_VID 0x1a2c
+#define QWERTY_PID 0x2c27
+
+int
+qwerty_found(struct xrt_prober *xp,
+           struct xrt_prober_device **devices,
+           size_t num_devices,
+           size_t index,
+           cJSON *attached_data,
+           struct xrt_device **out_xdevs);
+
+#ifdef __cplusplus
+}
+#endif
