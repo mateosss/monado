79f125acc22c075a9d7d3ac5d9b5a98846d099d1
Mateo de Mayo <mateodemayo@gmail.com>
2021-02-20 19:10:01 -0300

    Prepare for qwerty driver for WASD movement

--- src/xrt/drivers/qwerty/qwerty_driver.c
+++ src/xrt/drivers/qwerty/qwerty_driver.c
@@ -7,10 +7,20 @@
 #include "util/u_device.h"
 #include "util/u_distortion_mesh.h"
 
+#define QWERTY_INITIAL_MOVEMENT_SPEED 0.001f
 
 struct qwerty_hmd
 {
 	struct xrt_device base;
+	struct xrt_pose pose;
+
+	float movement_speed;
+	bool left_pressed;
+	bool right_pressed;
+	bool forward_pressed;
+	bool backward_pressed;
+	bool up_pressed;
+	bool down_pressed;
 };
 
 static inline struct qwerty_hmd *
@@ -22,8 +32,7 @@ qwerty_hmd(struct xrt_device *xdev)
 
 static void
 qwerty_update_inputs(struct xrt_device *xdev)
-{
-}
+{}
 
 static void
 qwerty_get_tracked_pose(struct xrt_device *xdev,
@@ -31,22 +40,25 @@ qwerty_get_tracked_pose(struct xrt_device *xdev,
                         uint64_t at_timestamp_ns,
                         struct xrt_space_relation *out_relation)
 {
-	// struct qwerty_hmd *qh = qwerty_hmd(xdev);
-
-	static float z = 0.f;
-
 	if (name != XRT_INPUT_GENERIC_HEAD_POSE) {
 		printf("[QWERTY ERROR] unknown input name");
 		return;
 	}
-	struct xrt_pose pose = {{0.0f, 0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}};
-	z += 0.001f;
-	pose.position.z = z;
-	// pose.orientation.y = 0;
-	pose.orientation.w = 1;
-	math_quat_normalize(&pose.orientation);
 
-	out_relation->pose = pose;
+	struct qwerty_hmd *qh = qwerty_hmd(xdev);
+
+	// clang-format off
+	if (qh->left_pressed) qh->pose.position.x -= qh->movement_speed;
+	if (qh->right_pressed) qh->pose.position.x += qh->movement_speed;
+	if (qh->forward_pressed) qh->pose.position.z -= qh->movement_speed;
+	if (qh->backward_pressed) qh->pose.position.z += qh->movement_speed;
+	if (qh->up_pressed) qh->pose.position.y += qh->movement_speed;
+	if (qh->down_pressed) qh->pose.position.y -= qh->movement_speed;
+	// clang-format on
+
+	math_quat_normalize(&qh->pose.orientation);
+
+	out_relation->pose = qh->pose;
 	out_relation->relation_flags = XRT_SPACE_RELATION_ORIENTATION_VALID_BIT |
 	                               XRT_SPACE_RELATION_POSITION_VALID_BIT |
 	                               XRT_SPACE_RELATION_ORIENTATION_TRACKED_BIT;
@@ -85,7 +97,6 @@ qwerty_destroy(struct xrt_device *xdev)
 	struct qwerty_hmd *qh = qwerty_hmd(xdev);
 
 	u_device_free(&qh->base);
-
 }
 
 int
@@ -101,6 +112,12 @@ qwerty_found(struct xrt_prober *xp,
 	enum u_device_alloc_flags flags = U_DEVICE_ALLOC_HMD | U_DEVICE_ALLOC_TRACKING_NONE;
 	size_t num_inputs = 1, num_outputs = 0;
 	struct qwerty_hmd *qh = U_DEVICE_ALLOCATE(struct qwerty_hmd, flags, num_inputs, num_outputs);
+
+	// Fill qwerty specific properties
+	qh->pose.orientation.w = 1.f;
+	qh->movement_speed = QWERTY_INITIAL_MOVEMENT_SPEED;
+
+	// Fill xrt_device properties
 	qh->base.name = XRT_DEVICE_GENERIC_HMD;
 	qh->base.device_type = XRT_DEVICE_TYPE_HMD;
 
@@ -156,3 +173,20 @@ qwerty_found(struct xrt_prober *xp,
 	out_xdevs[0] = &qh->base;
 	return 1;
 }
+
+// Emulated actions
+
+// clang-format off
+void qwerty_press_left(struct xrt_device *qh) { qwerty_hmd(qh)->left_pressed = true; }
+void qwerty_release_left(struct xrt_device *qh) { qwerty_hmd(qh)->left_pressed = false; }
+void qwerty_press_right(struct xrt_device *qh) { qwerty_hmd(qh)->right_pressed = true; }
+void qwerty_release_right(struct xrt_device *qh) { qwerty_hmd(qh)->right_pressed = false; }
+void qwerty_press_forward(struct xrt_device *qh) { qwerty_hmd(qh)->forward_pressed = true; printf(">>> qwerty_press_forward() qh=%p\n", (void*)qh);}
+void qwerty_release_forward(struct xrt_device *qh) { qwerty_hmd(qh)->forward_pressed = false; printf(">>> qwerty_release_forward()\n");}
+void qwerty_press_backward(struct xrt_device *qh) { qwerty_hmd(qh)->backward_pressed = true; }
+void qwerty_release_backward(struct xrt_device *qh) { qwerty_hmd(qh)->backward_pressed = false; }
+void qwerty_press_up(struct xrt_device *qh) { qwerty_hmd(qh)->up_pressed = true; }
+void qwerty_release_up(struct xrt_device *qh) { qwerty_hmd(qh)->up_pressed = false; }
+void qwerty_press_down(struct xrt_device *qh) { qwerty_hmd(qh)->down_pressed = true; }
+void qwerty_release_down(struct xrt_device *qh) { qwerty_hmd(qh)->down_pressed = false; }
+// clang-format on
