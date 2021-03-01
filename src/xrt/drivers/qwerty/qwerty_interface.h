135e45ad9675e3911dca9d68abad298ae83a0079
Mateo de Mayo <mateodemayo@gmail.com>
2021-03-01 16:37:31 -0300

    Migrate qwerty driver probing to an autoprober

--- src/xrt/drivers/qwerty/qwerty_interface.h
+++ src/xrt/drivers/qwerty/qwerty_interface.h
@@ -4,16 +4,8 @@
 extern "C" {
 #endif
 
-#define QWERTY_VID 0x1a2c
-#define QWERTY_PID 0x2c27
-
-int
-qwerty_found(struct xrt_prober *xp,
-             struct xrt_prober_device **devices,
-             size_t num_devices,
-             size_t index,
-             cJSON *attached_data,
-             struct xrt_device **out_xdevs);
+struct xrt_auto_prober *
+qwerty_create_auto_prober(void);
 
 void qwerty_press_left(struct xrt_device *qh);
 void qwerty_release_left(struct xrt_device *qh);
--- src/xrt/targets/common/target_lists.c
+++ src/xrt/targets/common/target_lists.c
@@ -109,10 +109,6 @@ struct xrt_prober_entry target_entry_list[] = {
     {VALVE_VID, VIVE_WATCHMAN_DONGLE_GEN2, vive_controller_found, "Valve Watchman Wireless Device", "vive"},
 #endif
 
-#ifdef XRT_BUILD_DRIVER_QWERTY
-    {QWERTY_VID, QWERTY_PID, qwerty_found, "QWERTY Keyboard", "qwerty"},
-#endif
-
     {0x0000, 0x0000, NULL, NULL, NULL}, // Terminate
 };
 
@@ -160,6 +156,10 @@ xrt_auto_prober_creator target_auto_list[] = {
 
 #ifdef XRT_BUILD_DRIVER_ILLIXR
     illixr_create_auto_prober,
+#endif
+
+#ifdef XRT_BUILD_DRIVER_QWERTY
+    qwerty_create_auto_prober,
 #endif
 
 #ifdef XRT_BUILD_DRIVER_DUMMY
