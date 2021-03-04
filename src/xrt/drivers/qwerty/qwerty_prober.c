748f43aa886b863df5043f5c1016cfcf6a49f5be
Mateo de Mayo <mateodemayo@gmail.com>
2021-03-04 10:01:48 -0300

    Use no_hmd property in qwerty driver.
    With this change, it is possible to use a different hmd with the qwerty controllers.
    Only tested with the dummy hmd.

--- src/xrt/drivers/qwerty/qwerty_prober.c
+++ src/xrt/drivers/qwerty/qwerty_prober.c
@@ -27,28 +27,30 @@ qwerty_prober_autoprobe(struct xrt_auto_prober *xap,
                         struct xrt_prober *xp,
                         struct xrt_device **out_xdevs)
 {
-	// TODO: Currently if no_hmd is true, nothing is created, but it would be
-	// better to detect if the user does not have an HMD, a left controller or a
-	// right controller and if the user requested it somehow, provide the
-	// remaining devices qwerty devices they might need.
-	if (no_hmds)
-		return 0;
+	bool hmd_wanted = !no_hmds; // Hopefully easier to reason about
 
-	struct qwerty_device *qhmd = qwerty_hmd_create();
-	struct qwerty_device *qctrl_left = qwerty_controller_create(true);
-	struct qwerty_device *qctrl_right = qwerty_controller_create(false);
+	// XXX: How fine grained can the user control what controllers/hmd combination
+	// they want? It would be nice to provide total control of that to the user.
+
+	struct qwerty_device *qhmd = hmd_wanted ? qwerty_hmd_create() : NULL;
+	struct qwerty_device *qctrl_left = qwerty_controller_create(qhmd, true);
+	struct qwerty_device *qctrl_right = qwerty_controller_create(qhmd, false);
 
 	// All devices should be able to reference other ones, qdevs should only be written here.
 	struct qwerty_devices qdevs = {qhmd, qctrl_left, qctrl_right};
-	qhmd->qdevs = qdevs;
+
+	if (hmd_wanted) {
+		qhmd->qdevs = qdevs;
+		out_xdevs[0] = &qhmd->base;
+	}
+
 	qctrl_left->qdevs = qdevs;
 	qctrl_right->qdevs = qdevs;
 
-	out_xdevs[0] = &qhmd->base;
-	out_xdevs[1] = &qctrl_left->base;
-	out_xdevs[2] = &qctrl_right->base;
+	out_xdevs[1 - !hmd_wanted] = &qctrl_left->base;
+	out_xdevs[2 - !hmd_wanted] = &qctrl_right->base;
 
-	int num_qwerty_devices = 3;
+	int num_qwerty_devices = hmd_wanted + 2;
 	return num_qwerty_devices;
 }
 
