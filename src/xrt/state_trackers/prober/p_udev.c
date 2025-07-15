mateosss/monado

commit 445829f6ce84f402b983f73e2eedfc02f6c0cd21

st/prober: Add check for virtual hidraw devices
Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2336>

src/xrt/state_trackers/prober/p_udev.c
@@ -369,6 +369,14 @@ p_udev_add_v4l(struct prober_device *pdev, uint32_t v4l_index, uint32_t usb_ifac
 #endif
 }
 
+static bool
+device_is_virtual(struct udev_device *raw_dev)
+{
+	const char *prop_id_path = udev_device_get_property_value(raw_dev, "ID_PATH");
+
+	return (prop_id_path == NULL);
+}
+
 static void
 p_udev_enumerate_hidraw(struct prober *p, struct udev *udev)
 {
@@ -388,7 +396,7 @@ p_udev_enumerate_hidraw(struct prober *p, struct udev *udev)
 		struct udev_device *raw_dev = NULL;
 		uint16_t vendor_id;
 		uint16_t product_id;
-		uint16_t interface;
+		uint16_t interface = 0;
 		uint8_t dev_class = 0;
 		uint16_t usb_bus = 0;
 		uint16_t usb_addr = 0;
@@ -428,14 +436,16 @@ p_udev_enumerate_hidraw(struct prober *p, struct udev *udev)
 		default: P_ERROR(p, "Unknown hidraw bus_type: '%i', ignoring.", bus_type); goto next;
 		}
 
-		// HID interface.
-		ret = p_udev_get_interface_number(raw_dev, &interface);
-		if (ret != 0) {
-			P_ERROR(p,
-			        "In enumerating hidraw devices: "
-			        "Failed to get interface number for '%s'",
-			        sysfs_path);
-			goto next;
+		// Get USB interface number for non-virtual devices.
+		if (!device_is_virtual(raw_dev)) {
+			ret = p_udev_get_interface_number(raw_dev, &interface);
+			if (ret != 0) {
+				P_ERROR(p,
+				        "In enumerating hidraw devices: "
+				        "Failed to get interface number for '%s'",
+				        sysfs_path);
+				goto next;
+			}
 		}
 
 		if (bus_type == HIDRAW_BUS_BLUETOOTH) {

commit d2437ff1dc624ccd1f1ff60f168c5889fc5e3651

st/prober: always try to resolve interface ID
The commit 445829f6ce84 ("st/prober: Add check for virtual hidraw
devices") added a check for ID_PATH. However, this is a udev property
that is set by rules and may not always be set for actual physical
devices.

Quite likely, one could just move the check into the HIDRAW_BUS_USB.
But, it is also perfectly save to ignore the error if the device does
not have a parent in the USB subsystem. So do that for now.

Fixes: 445829f6ce84 ("st/prober: Add check for virtual hidraw device")

src/xrt/state_trackers/prober/p_udev.c
@@ -437,15 +437,14 @@ p_udev_enumerate_hidraw(struct prober *p, struct udev *udev)
 		}
 
 		// Get USB interface number for non-virtual devices.
-		if (!device_is_virtual(raw_dev)) {
-			ret = p_udev_get_interface_number(raw_dev, &interface);
-			if (ret != 0) {
-				P_ERROR(p,
-				        "In enumerating hidraw devices: "
-				        "Failed to get interface number for '%s'",
-				        sysfs_path);
-				goto next;
-			}
+		ret = p_udev_get_interface_number(raw_dev, &interface);
+		// -1 is returned if it is not a USB device
+		if (ret < -1) {
+			P_ERROR(p,
+			        "In enumerating hidraw devices: "
+			        "Failed to get interface number for '%s'",
+			        sysfs_path);
+			goto next;
 		}
 
 		if (bus_type == HIDRAW_BUS_BLUETOOTH) {
@@ -545,7 +544,7 @@ p_udev_get_interface_number(struct udev_device *raw_dev, uint16_t *interface)
 
 	str = udev_device_get_sysattr_value(intf_dev, "bInterfaceNumber");
 	if (str == NULL) {
-		return -1;
+		return -2;
 	}
 
 	*interface = (uint16_t)strtol(str, NULL, 16);